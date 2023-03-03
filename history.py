bde43ed46b761216040baff41aede5a1fba9dc30
Aytage <aytacagayeva18@gmail.com>
2023-03-02 23:07:58 -0800

    Header elave edildi

---
+++ history.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Tue Feb 28 04:25:46 2023)---
+runfile('C:/Users/Aytac Agayev/.spyder-py3/temp.py', wdir='C:/Users/Aytac Agayev/.spyder-py3')
+
+## ---(Tue Feb 28 04:27:54 2023)---
+streamlit hello
+
+## ---(Tue Feb 28 04:46:57 2023)---
+streamlit run temp.py
+
+## ---(Tue Feb 28 21:35:51 2023)---
+runfile('C:/Users/Aytac Agayev/.spyder-py3/temp.py', wdir='C:/Users/Aytac Agayev/.spyder-py3')
